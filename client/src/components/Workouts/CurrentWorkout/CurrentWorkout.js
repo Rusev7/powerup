a288efd8b8d104016cb4a7369ab99cf9f1c6f36b
RRusev7 <skeletonstepbg@gmail.com>
2021-03-30 19:54:27 +0300

    Finished my workouts page

--- client/src/components/Workouts/CurrentWorkout/CurrentWorkout.js
+++ client/src/components/Workouts/CurrentWorkout/CurrentWorkout.js
@@ -1,15 +1,18 @@
+import { useState } from 'react';
+
 import './CurrentWorkout.css';
 
 const CurrentWorkout = ({
     workout
 }) => {
+    const [currentExercise, setCurrentExercise] = useState(null);
+    console.log(currentExercise);
+
     if(!workout) {
         return null;
     } else {
         let date = new Date(workout.date);
         date = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-
-        console.log(workout);
 
         return (
             <form className="workout-container">
@@ -21,13 +24,24 @@ const CurrentWorkout = ({
                         <h2 className="exercises-list-heading">Exercises:</h2>
                         {workout.exercises.map((exercise, index) => {
                             return (
-                                <span key={index} className="exercise-list-item">&#10146; {exercise.name}</span>
+                                <div key={index} className="exercise-list-item">
+                                    <span  className="exercise-list-item-text">&#10146; {exercise.name}</span>
+                                    <button type="button" onClick={() => setCurrentExercise(workout.exercises[index])} className="exercise-list-item-btn">details</button>
+                                </div>
                             )
                         })}
                     </div>
-                    <div className="current-exercise-container">
-
-                    </div>
+                    {currentExercise && <div className="current-exercise-container">
+                        <h3 className="current-exercise-heading">{currentExercise.name}</h3>
+                        {currentExercise.sets.map((set, index) => (
+                            <div key={index + 10} className="current-exercise-set-row">
+                                <span className="current-exercise-info">Set {index + 1}:</span>
+                                <span className="current-exercise-info">Reps: {set.reps}</span>
+                                <span className="current-exercise-info">Weight: {set.weight}</span>
+                            </div>
+                        ))}
+                    </div>}
+                    
                 </div>
             </form>
         )
